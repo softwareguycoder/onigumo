72d9d0b2979102ea589756e7233703f28fffdb4f
softwareguycoder <softwareguycoder@gmail.com>
2019-06-02 09:39:57 -0400

    Extracted menu-creation functionality into new private methods

--- inuyasha/commands/command_target.py
+++ inuyasha/commands/command_target.py
@@ -9,7 +9,33 @@ from translators.ps_exec_output_line_to_function_item_translator \
 
 class CommandTarget(object):
     session = None
+
+    @staticmethod
+    def __CreateProcessPickerMenu():
+        menu = ConsoleMenu(APP_TITLE, "Pick a process to kill", 
+            show_exit_option=True)
+        if len(CommandTarget.session.GetResponseLines()) > 0:
+            for line in CommandTarget.session.GetResponseLines()[1:].strip():
+                new_menu_item = \
+                    PsExecOutputLineToFunctionItemTranslator \
+                        .ToFunctionItem(line)
+                if new_menu_item is None:
+                    continue
+                menu.append_item(new_menu_item)
+        menu.show()
+        pass
+
     
+    @staticmethod
+    def __CreateServerFunctionsMenu():
+        menu = ConsoleMenu(APP_TITLE, APP_SUBTITLE, show_exit_option=True)
+        menu.append_item(FunctionItem(IDM_SERVER_LIST_PROCESSES,
+            CommandTarget.OnListProcessesOnRemoteMachine))
+        menu.append_item(FunctionItem("End Session", 
+            CommandTarget.OnEndSession))
+        menu.show()
+
+
     @staticmethod
     def OnConnectRemoteMachine():
         CommandTarget.session = SessionManager(TCP_IP, TCP_PORT)
@@ -17,12 +43,7 @@ class CommandTarget(object):
         if not CommandTarget.session.Open():           
             print("ERROR: Failed to connect to remote server.")
             exit(EXIT_FAILURE)
-        menu = ConsoleMenu(APP_TITLE, APP_SUBTITLE, show_exit_option=True)
-        menu.append_item(FunctionItem(IDM_SERVER_LIST_PROCESSES,
-                                      CommandTarget.OnListProcessesOnRemoteMachine))
-        menu.append_item(FunctionItem("End Session", 
-                                      CommandTarget.OnEndSession))
-        menu.show()
+        CommandTarget.__CreateServerFunctionsMenu()
         pass
     
     @staticmethod
@@ -35,16 +56,7 @@ class CommandTarget(object):
         if not CommandTarget.session:
             exit(EXIT_FAILURE)
         CommandTarget.session.ListRemoteProcesses()
-        menu = ConsoleMenu(APP_TITLE, "Pick a process to kill", show_exit_option=True)
-        if len(CommandTarget.session.GetResponseLines()) > 0:
-            for line in CommandTarget.session.GetResponseLines()[1:].strip():
-                new_menu_item = \
-                    PsExecOutputLineToFunctionItemTranslator \
-                        .ToFunctionItem(line)
-                if new_menu_item is None:
-                    continue
-                menu.append_item(new_menu_item)
-        menu.show()
+        CommandTarget.__CreateProcessPickerMenu()
         pass
     
     @staticmethod
