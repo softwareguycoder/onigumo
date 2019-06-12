8686743a842de2343fef73d65e0d7c2d486b0925
softwareguycoder <softwareguycoder@gmail.com>
2019-06-12 08:26:14 -0400

    Added MULTILINE_START and MULTILINE_END values

--- server/include/invocation_status.h
+++ server/include/invocation_status.h
@@ -6,7 +6,9 @@ typedef enum {
   OPENED, /* command invocation session has begun */
   ERROR,  /* command utterly failed to carry out its objective */
   WARNING, /* user supplied incorrect input, or other non-showstopper  */
-  WAITING, /* command engine waiting for new input from user */
+  WAITING, /* command engine waiting for new input from user/client */
+  MULTILINE_START,   /* starting a multiline reply */
+  MULITLINE_END,     /* ending a multiline reply */
 }  INVOCATIONSTATUS, *LPINVOCATIONSTATUS;
 
 #endif //__INVOCATION_STATUS_H__
