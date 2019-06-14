96d3ab61e8822c010c9ed733e98b5a33bc46dab5
softwareguycoder <softwareguycoder@gmail.com>
2019-06-14 08:19:37 -0400

    Added FormatEltsSeparatedBy method to ListUtilities

--- inuyasha/common/list_utils.py
+++ inuyasha/common/list_utils.py
@@ -1,4 +1,11 @@
+from common.string_utils import StringUtilities
 class ListUtilities(object):
+    @staticmethod
+    def FormatEltsSeparatedBy(char, theList):
+        if StringUtilities.IsNullOrWhiteSpace(char):
+            return ''
+        return str(char).join(map(str, theList))
+    
     @staticmethod
     def PrintEltsSeparatedBy(char, theList):
         print(str(char).join(map(str,theList)))
