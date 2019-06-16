7a31dbdb5a064ba8226ac926f8e069e7c46bfafa
softwareguycoder <softwareguycoder@gmail.com>
2019-06-16 10:22:21 -0400

    Change ReplyToClient to track bytes sent and return void

--- server/src/client_manager.c
+++ server/src/client_manager.c
@@ -15,70 +15,74 @@
 //
 
 void ForciblyDisconnectClient(LPCLIENTSTRUCT lpCS) {
-	// lpCS is the reference to the structure containing
-	// information for the client whose connection you want to sever
+  // lpCS is the reference to the structure containing
+  // information for the client whose connection you want to sever
 
-	if (lpCS == NULL) {
-		// Null value provided for the client structure; nothing to do.
-		return;
-	}
+  if (lpCS == NULL) {
+    // Null value provided for the client structure; nothing to do.
+    return;
+  }
 
-	/* Check whether there is still a valid socket file descriptor
-	 * available for the client endpoint... */
-	if (!IsSocketValid(lpCS->nSocket)) {
-		// Invalid socket file descriptor available for this client; nothing
-		// to do.
-		return;
-	}
+  /* Check whether there is still a valid socket file descriptor
+   * available for the client endpoint... */
+  if (!IsSocketValid(lpCS->nSocket)) {
+    // Invalid socket file descriptor available for this client; nothing
+    // to do.
+    return;
+  }
 
-	if (lpCS->bConnected == FALSE) {
-		// Nothing to do if the client is already marked as
-		// not connected
-		return;
-	}
+  if (lpCS->bConnected == FALSE) {
+    // Nothing to do if the client is already marked as
+    // not connected
+    return;
+  }
 
-	fprintf(stdout, SERVER_DATA_FORMAT, ERROR_FORCED_DISCONNECT);
+  fprintf(stdout, SERVER_DATA_FORMAT, ERROR_FORCED_DISCONNECT);
 
-	/* Forcibly close client connections */
-	Send(lpCS->nSocket, ERROR_FORCED_DISCONNECT);
-	CloseSocket(lpCS->nSocket);
+  /* Forcibly close client connections */
+  Send(lpCS->nSocket, ERROR_FORCED_DISCONNECT);
+  CloseSocket(lpCS->nSocket);
 
-	LogInfo(CLIENT_DISCONNECTED, lpCS->szIPAddress, lpCS->nSocket);
+  LogInfo(CLIENT_DISCONNECTED, lpCS->szIPAddress, lpCS->nSocket);
 
-	if (GetLogFileHandle() != stdout) {
-		fprintf(stdout, CLIENT_DISCONNECTED, lpCS->szIPAddress, lpCS->nSocket);
-	}
+  if (GetLogFileHandle() != stdout) {
+    fprintf(stdout, CLIENT_DISCONNECTED, lpCS->szIPAddress, lpCS->nSocket);
+  }
 
-	/* set the client socket file descriptor to now have a value of -1,
-	 * since its socket has been closed and we've said good bye.  This will
-	 * prevent any other socket functions from working on this now dead socket.
-	 */
-	lpCS->nSocket = INVALID_SOCKET_VALUE;
-	lpCS->bConnected = FALSE;
+  /* set the client socket file descriptor to now have a value of -1,
+   * since its socket has been closed and we've said good bye.  This will
+   * prevent any other socket functions from working on this now dead socket.
+   */
+  lpCS->nSocket = INVALID_SOCKET_VALUE;
+  lpCS->bConnected = FALSE;
 }
 
-int ReplyToClient(LPCLIENTSTRUCT lpCS, const char* pszBuffer) {
-	int nBytesSent = SendToClient(lpCS, pszBuffer);
-	if (nBytesSent < 0) {
-	  fprintf(stderr, "ERROR: Send has failed.\n");
-	  perror("server[ReplyToClient]");
+void ReplyToClient(LPCLIENTSTRUCT lpCS, const char* pszBuffer) {
+  if (!IsClientStructValid(lpCS)) {
+    fprintf(stderr, "ERROR: Client structure invalid.\n");
+    return;
+  }
 
-		FreeSocketMutex();
+  int nBytesSent = SendToClient(lpCS, pszBuffer);
+  if (nBytesSent < 0) {
+    fprintf(stderr, "ERROR: Send has failed.\n");
+    perror("server[ReplyToClient]");
 
-		CleanupServer(ERROR);
+    FreeSocketMutex();
 
-		return -1;
-	}
+    CleanupServer(ERROR);
+  }
 
-	// Asume buffer terminates in a newline.  Report what the server
-	// is sending to the console and the log file.  Only log the server
-	// as successfully having sent a message if and only if a message was
-	// actually sent!
-	fprintf(stdout, SERVER_DATA_FORMAT, pszBuffer);
+  // Asume buffer terminates in a newline.  Report what the server
+  // is sending to the console and the log file.  Only log the server
+  // as successfully having sent a message if and only if a message was
+  // actually sent!
+  fprintf(stdout, SERVER_DATA_FORMAT, pszBuffer);
 
-	if (GetLogFileHandle() != stdout) {
-		LogInfo(SERVER_DATA_FORMAT, pszBuffer);
-	}
+  if (GetLogFileHandle() != stdout) {
+    LogInfo(SERVER_DATA_FORMAT, pszBuffer);
+  }
 
-	return nBytesSent;
+  /* track the nBytesSent with the corresponding member of LPCLIENTSTRUCT */
+  lpCS->nBytesSent += nBytesSent;
 }
